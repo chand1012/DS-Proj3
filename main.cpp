4dc70aea80430f8fd89f9f2e8de939f34ac3f3fc
chand1012 <chandler@chand1012.dev>
2021-04-20 16:17:08 -0400

    Add helper functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include "HashTable.hpp"
+#include "helpers.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
 
 int main() {
+
+    srand(time(NULL));
+
+    int iterations = 10000;
+    string charset = "012";
+    int length;
+
+    vector<string> test_strings;
+
+    for (int i = 0; i < iterations; ++i) {
+        length = rand() % 10 + 1;
+        test_strings.push_back(randString(length));
+    }
+
+    HashTable table;
+
+    for (int i = 0; i < iterations; ++i) {
+        string test = test_strings.at(i);
+        table.add(test, i);
+    }
+
+
     return 0;
 }
