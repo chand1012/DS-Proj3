bb08c9b39f5619011072478c267c8a65c81444df
Chandler Lofland <chandler@chand1012.dev>
2021-04-22 15:52:20 -0400

    Get test running

--- main.cpp
+++ main.cpp
@@ -6,15 +6,12 @@
 #include <vector>
 #include <chrono>
 #include <iomanip>
+#include <sstream>
 
 #include "HashTable.hpp"
 #include "helpers.hpp"
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-using std::ofstream;
+using namespace std;
 
 /*
     The tests will be as follows:
@@ -33,6 +30,15 @@ int main() {
     string tempStr;
     vector<string> randomStrings;
     ofstream output("Results.csv");
+    stringstream add;
+    stringstream get;
+    stringstream remove;
+
+    add << "Array Hash Table Add,";
+    get << "Array Hash Table Get,";
+    remove << "Array Hash Table Remove,";
+
+
 
     output << "Test Name,10,100,1000,10000,100000" << endl;
 
@@ -67,22 +73,35 @@ int main() {
         }
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        add << duration.count() << ",";
         cout << "Done." << endl;
         cout << "Running get tests." << endl;
+        start = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < iteration/2; ++j) {
             rnd = randint(0, iteration-1);
             tempStr = test.get(testStrings[rnd]);
         }
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        get << duration.count() << ",";
         cout << "Done." << endl;
         cout << "Running remove tests." << endl;
+        start = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < iteration/2; ++j) {
             rnd = randint(0, iteration-1);
             tempStr = test.remove(testStrings[rnd]);
         }
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        remove << duration.count() << ",";
         cout << "Done." << endl;
         cout << "Finished all tests for " << iteration << " iterations." << endl;
         cout << std::setfill('-') << std::setw(19) << '-' << endl;
     }
+
+    output << add.str() << endl;
+    output << get.str() << endl;
+    output << remove.str() << endl;
 
     output.close();
 
